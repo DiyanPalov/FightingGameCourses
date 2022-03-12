4ba55bb9fbbb07bffe98f4d2b0d124044a542992
DiyanPalov <ikokos@abv.bg>
2022-03-12 19:03:06 +0200

    Create and scaffold category item controller

--- FightingGameCourses/Data/Migrations/20220312160259_Add_Description_To_CategoryItem_Table.Designer.cs
+++ FightingGameCourses/Data/Migrations/20220312160259_Add_Description_To_CategoryItem_Table.Designer.cs
@@ -4,14 +4,16 @@ using FightingGameCourses.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FightingGameCourses.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220312160259_Add_Description_To_CategoryItem_Table")]
+    partial class Add_Description_To_CategoryItem_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
